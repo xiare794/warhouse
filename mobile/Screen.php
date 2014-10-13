ab1c4aac7eb09b2bafbf9ba235ade41723a01d01
xiare794 <18139437@qq.com>
2014-10-13 18:03:50 +0800

    桌面版Fix
    1.coreFunction 签署功能丢失
    2.增加刷新显示的Screen.php

---
+++ mobile/Screen.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <title>Ware House Mobile Cient</title>
+    <link rel="stylesheet" href="jquery-mobile/jquery.mobile-1.4.0.min.css" />
+    <script class="include" type="text/javascript" src="jquery-mobile/jquery-1.9.1.js"></script>
+    <script src="jquery-mobile/jquery.mobile-1.4.0.min.js"></script> 
+   </head>
+
+   <body>
+   </body>
+
+</html>
